3e27145f953604d26e8207c2a23943b2689316a6
lara <larasaad270@gmail.com>
2026-05-20 14:20:00 +0300

    adding the sequence add on

---
+++ real_estate_project/real_estate/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Real Estate Test',
+    'version': '0.1',
+    'summary': 'real estate workflow',
+    'description': 'Long description of module purpose',
+    'author': 'Lara',
+    'website': 'https://www.yourcompany.com',
+    'category': 'Uncategorized',
+    'depends': ['base','hr'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
